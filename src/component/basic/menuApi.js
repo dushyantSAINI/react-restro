dd4a5dde68c592314f2b837958ecf33741a624e7
dushyant saini <dushyant.sjai@gmail.com>
2022-09-13 16:43:30 +0530

    deploy my app

---
+++ src/component/basic/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        " 2 Minutes Noodles and are very Delicous",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "aloo pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        " Very Tasty ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Very light meal  ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Delicious and best for lunch  ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "A good evening snack ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Special item of the day ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Made with Pure milk ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        " Good quality Basmati Rice ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        " Light snack ",
+    },
+  ];
+  
+  export default Menu;
+  
